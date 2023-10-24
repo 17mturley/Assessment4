46d68f88bcaedd71d6d901eb46e62f857afb231e
Megan Turley <143685024+17mturley@users.noreply.github.com>
2023-10-24 14:22:48 -0600

    Assessment 4

---
+++ controller.js
@@ -0,0 +1,41 @@
+const cats = require("./db.json")
+let globalId = 4
+
+module.exports = {
+getCompliment: (req, res) => {
+    const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
+
+    res.status(200).send(randomCompliment);
+},
+getFortune: (req, res) => {
+    const fortunes = ["Your mind is creative, original and alert.", "Your difficulties will strengthen you.", "The one that recognizes the illusion does not act as if it is real.", "The strong person understands how to withstand substantial loss.", "Swimming is easy. Stay floating is hard."];
+
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex];
+
+    res.status(200).send(randomFortune);
+},
+getCats: (req, res) => res.status(200).send(cats),
+deleteCat: (req, res) => {
+    let index = findIndex(elem => elem.id === +req.params.id);
+    splice(index, 1);
+    res.status(200).send(cats);
+},
+createCat: (req, res) => {
+    let { name, nickname, rating, imageURL } = req.body;
+    let newCat = {
+        id: globalId,
+        name,
+        nickname,
+        rating,
+        imageURL
+    };
+    push(newCat);
+    res.status(200).send(cats);
+    globalId++;
+}
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+const {
+    getCompliment,
+    getFortune,
+    getCats,
+    deleteCat,
+    createCat
+} = require('./controller');
+
+app.get("/api/compliment", getCompliment);
+app.get("/api/fortune", getFortune);
+app.get(`/api/cats`, getCats);
+app.delete(`/api/cats/:id`, deleteCat);
+app.post(`/api/cats`, createCat);
+
+app.listen(4000, () => console.log("Server running on 4000"));
